8370a503fd8782c17168fad520ed593f6168279e
neobats <batton.guy@gmail.com>
2022-07-01 21:50:19 -0400

    actually added link now

---
+++ components/themed/Link.tsx
@@ -0,0 +1,19 @@
+import NextLink from "next/link"
+import { Link as ChakraLink } from "@chakra-ui/react"
+
+type Props = {
+  children: React.ReactNode
+  href: string
+  color?: string
+}
+// https://chakra-ui.com/docs/styled-system/color-mode#usecolormodevalue
+export const Link = (props: Props) => {
+
+  return (
+    <NextLink href={props.href} passHref>
+      <ChakraLink color="teal.200" _active={{ color: "teal.100" }}>
+        {props.children}
+      </ChakraLink>
+    </NextLink>
+  )
+}
