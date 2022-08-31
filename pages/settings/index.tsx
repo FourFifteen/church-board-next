0f3283fa03cfec6d53b3ceb91849a1a42df2090b
neobats <batton.guy@gmail.com>
2022-08-31 18:36:08 -0400

    lint

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -6,15 +6,21 @@ import {
   Heading,
   Stack,
   Text,
-  useColorMode
-} from "@chakra-ui/react";
-import { NextPage } from "next";
-import { Control, FieldError, FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { RiImageAddFill, RiMoonClearFill, RiSunFill } from "react-icons/ri";
-import EditableWithButton from "../../components/themed/EditableWithButton";
-import FileUpload from "../../components/themed/FileUpload";
-import HookFormControl from "../../components/themed/HookFormControl";
-import { useProtectedRouteAuth } from "../../hooks/useProtectedRouteAuth";
+  useColorMode,
+} from "@chakra-ui/react"
+import { NextPage } from "next"
+import {
+  Control,
+  FieldError,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form"
+import { RiImageAddFill, RiMoonClearFill, RiSunFill } from "react-icons/ri"
+import EditableWithButton from "../../components/themed/EditableWithButton"
+import FileUpload from "../../components/themed/FileUpload"
+import HookFormControl from "../../components/themed/HookFormControl"
+import { useProtectedRouteAuth } from "../../hooks/useProtectedRouteAuth"
 
 export type UpdateUserInputs = {
   name: string
@@ -25,30 +31,33 @@ export type UpdateUserInputs = {
 // I'll need to make this separate at some point...
 const THEME_ICON_MAP = {
   dark: <RiMoonClearFill />,
-  light: <RiSunFill />
+  light: <RiSunFill />,
 }
 
 const SettingsIndexPage: NextPage = () => {
   const { currentUser } = useProtectedRouteAuth()
   const { colorMode, toggleColorMode } = useColorMode()
-  const { register, handleSubmit, formState, control } = useForm<UpdateUserInputs>()
+  const { register, handleSubmit, formState, control } =
+    useForm<UpdateUserInputs>()
 
   const {
     name: nameErrors,
     email: emailErrors,
-    photoURL: photoErrors
+    photoURL: photoErrors,
   } = formState.errors
 
-  const postUserSettings: SubmitHandler<UpdateUserInputs> = async (formData) => {
+  const postUserSettings: SubmitHandler<UpdateUserInputs> = async (
+    formData,
+  ) => {
     if (!currentUser) {
       return
     }
     const response = await fetch("api/users/" + currentUser.id, {
       method: "PATCH",
       headers: {
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify(formData),
     })
     const data = response.json()
     console.log(data)
@@ -56,51 +65,65 @@ const SettingsIndexPage: NextPage = () => {
 
   return (
     <Container>
-      <Stack
-        direction="column"
-        py={8}
-        w="full"
-        justifyContent="start"
-      >
+      <Stack direction="column" py={8} w="full" justifyContent="start">
         <Heading as="h1">Settings</Heading>
-        <Text>Here&apos;s where you can review and update your profile settings.</Text>
+        <Text>
+          Here&apos;s where you can review and update your profile settings.
+        </Text>
       </Stack>
       {currentUser && (
-
-        <Grid
-          templateColumns={["2fr 1fr", "1fr"]}
-        >
+        <Grid templateColumns={["2fr 1fr", "1fr"]}>
           <GridItem w="full">
             <form onSubmit={handleSubmit(postUserSettings)}>
-              <Stack
-                spacing="6"
-                w="full"
-              >
+              <Stack spacing="6" w="full">
                 <HookFormControl error={nameErrors} label="Name">
-                  <EditableWithButton defaultValue={currentUser.name} {...register("name")} />
+                  <EditableWithButton
+                    defaultValue={currentUser.name}
+                    {...register("name")}
+                  />
                 </HookFormControl>
                 <HookFormControl error={emailErrors} label="Email">
-                  <EditableWithButton defaultValue={currentUser.email} type="email" {...register("email")} />
+                  <EditableWithButton
+                    defaultValue={currentUser.email}
+                    type="email"
+                    {...register("email")}
+                  />
                 </HookFormControl>
-                <HookFormControl error={photoErrors as FieldError | undefined} label="Profile Photo">
-                  <FileUpload accept="image/*" control={control as unknown as Control<FieldValues, any>} name="photoURL" label="Profile Photo">
-                    <Button leftIcon={<RiImageAddFill />}>Upload Profile Image</Button>
+                <HookFormControl
+                  error={photoErrors as FieldError | undefined}
+                  label="Profile Photo"
+                >
+                  <FileUpload
+                    accept="image/*"
+                    control={control as unknown as Control<FieldValues, any>}
+                    name="photoURL"
+                    label="Profile Photo"
+                  >
+                    <Button leftIcon={<RiImageAddFill />}>
+                      Upload Profile Image
+                    </Button>
                   </FileUpload>
                 </HookFormControl>
-                <Button colorScheme="teal" variant="solid" type="submit">Save changes</Button>
+                <Button colorScheme="teal" variant="solid" type="submit">
+                  Save changes
+                </Button>
               </Stack>
             </form>
           </GridItem>
           <GridItem ml={[6, 0]} mt={[0, 6]}>
-            <Button onClick={toggleColorMode} id="colormode" colorScheme="teal" variant="outline">
+            <Button
+              onClick={toggleColorMode}
+              id="colormode"
+              colorScheme="teal"
+              variant="outline"
+            >
               {THEME_ICON_MAP[colorMode]}
             </Button>
           </GridItem>
         </Grid>
       )}
-    </Container >
+    </Container>
   )
 }
 
 export default SettingsIndexPage
-
