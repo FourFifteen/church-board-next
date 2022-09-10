6e7ac8062294472b89ef448884e7b4f3b14ddbad
neobats <batton.guy@gmail.com>
2022-09-09 21:31:38 -0400

    generic control. Added label.

--- components/themed/HookFormControl.tsx
+++ components/themed/HookFormControl.tsx
@@ -2,17 +2,22 @@ import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/react"
 import { FieldError } from "react-hook-form"
 
 type HookFormControlProps = {
+  children: React.ReactNode
   error?: FieldError
   label?: string
-  children: React.ReactNode
+  name: string
 }
-const HookFormControl = ({ children, error, label }: HookFormControlProps) => (
+const HookFormControl = ({
+  children,
+  error,
+  label,
+  name,
+}: HookFormControlProps) => (
   <FormControl isInvalid={Boolean(error)}>
-    {label && <FormLabel>{label}</FormLabel>}
+    {label && <FormLabel htmlFor={name}>{label}</FormLabel>}
     {children}
     {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
   </FormControl>
 )
 
 export default HookFormControl
-
